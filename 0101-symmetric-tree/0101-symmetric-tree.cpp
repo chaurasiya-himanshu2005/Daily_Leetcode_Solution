e25eadc483abd3e23e4de92b332ea0f4f625e2c2
Himanshu Chaurasiya <chaurasiya.himanshu2005@gmail.com>
2026-09-08 15:23:43 +0530

    Time: 0 ms (100%), Space: 18.6 MB (14.11%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,22 +11,13 @@
  */
 class Solution {
 public:
-    void invert(TreeNode* root){
-        if(root == NULL) return;
-        swap(root->left, root->right);
-        invert(root->left);
-        invert(root->right);
-    }
-    bool isSameTree(TreeNode* p, TreeNode* q) {
+    bool areInverts(TreeNode* p, TreeNode* q){
         if(p == NULL && q == NULL) return true;
         if(p == NULL || q == NULL) return false;
         if(p->val != q->val) return false;
-        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        return areInverts(p->left,q->right) && areInverts(p->right,q->left);
     }
     bool isSymmetric(TreeNode* root) {
-        invert(root->left);
-        bool ans = isSameTree(root->left,root->right);
-        invert(root->left);
-        return ans;
+        return areInverts(root->left, root->right);
     }
 };
