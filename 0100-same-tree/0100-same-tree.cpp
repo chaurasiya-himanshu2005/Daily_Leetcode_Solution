7a639880ce37a123f538a526fe90b3e88e3bc9bb
Himanshu Chaurasiya <chaurasiya.himanshu2005@gmail.com>
2026-09-10 09:40:28 +0530

    Time: 0 ms (100%), Space: 12.8 MB (47.34%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -15,6 +15,7 @@ public:
         if(p == NULL && q == NULL) return true;
         if(p == NULL || q == NULL) return false;
         if(p->val != q->val) return false;
-        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        bool ans = isSameTree(p->left, q->left) and isSameTree(p->right,q->right);
+        return ans;
     }
 };
