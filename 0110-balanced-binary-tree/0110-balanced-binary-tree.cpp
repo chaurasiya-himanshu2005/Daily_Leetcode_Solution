a87939dcef646568295a574af66245cd09348d33
Himanshu Chaurasiya <chaurasiya.himanshu2005@gmail.com>
2026-09-10 10:48:28 +0530

    Time: 0 ms (100%), Space: 22.9 MB (83.49%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,17 +11,15 @@
  */
 class Solution {
 public:
-    bool ans;
     int levels(TreeNode* root){
         if(root == NULL) return 0;
-        int left = levels(root->left);
-        int right = levels(root->right);
-        if(abs(left-right) > 1) ans = false;
-        return 1 + max(left, right);
+        return 1 + max(levels(root->left), levels(root->right));
     }
     bool isBalanced(TreeNode* root) {
-        ans = true; //true means balanced
-        levels(root);
-        return ans;
+        if(root == NULL) return true;
+        int lstLevels = levels(root->left);
+        int rstLevels = levels(root->right);
+        if(abs(lstLevels - rstLevels) > 1) return false;
+        return isBalanced(root->left) && isBalanced(root->right);
     }
 };
