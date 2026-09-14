5f7f46fb96b50863c6f53bcfdb1c8fbee7569710
Himanshu Chaurasiya <chaurasiya.himanshu2005@gmail.com>
2026-09-14 19:38:31 +0530

    Time: 0 ms (100%), Space: 9.8 MB (47.04%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        int x1 = rec1[0], y1 = rec1[1], x2 = rec1[2], y2 = rec1[3];
+        int a1 = rec2[0], b1 = rec2[1], a2 = rec2[2], b2 = rec2[3];
+
+        if(x2 <= a1 || a2 <= x1 || y2 <= b1 || b2 <= y1) return false;
+        return true;
+    }
+};
